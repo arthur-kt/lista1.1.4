09ae0d5f596772eef7eae11570257038b53096ed
Arthur Kafrouni Tomazi <arthur.k.t@hotmail.com>
2023-06-30 15:25:29 -0300

    Add files via upload

---
+++ lista1.1.4.py
@@ -0,0 +1,22 @@
+"""
+Lista 1, parte 1, exercício 4
+
+Descrição: Este programa apresenta a resolução de uma questão da lista 1 da cadeira de Economia Computacional
+Autor:Arthur Kafrouni Tomazi
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+nome = input("Qual é o seu nome? ")
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+print("Oi,", nome)
